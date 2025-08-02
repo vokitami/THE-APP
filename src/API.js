e9b6b5e386fa3c3e6b56abf07cad6cb80a8c0e9b
vokitami <annawe407@gmail.com>
2025-08-01 20:42:23 -0700

    update API.js

--- src/API.js
+++ src/API.js
@@ -4,4 +4,13 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
+// 👇 Interceptor que agrega el token a cada petición automáticamente
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default API;
